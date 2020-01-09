a81177e5b35ac071d55ebebfa57354dcd32f2f55
Fhiz <40817126+wovencode@users.noreply.github.com>
2020-01-09 13:31:35 +0100

    Refactor to prepare for mySQL
    Refactor to prepare for mySQL

---
+++ Scripts/Layers/DatabaseLayerMySQL.cs
@@ -0,0 +1,30 @@
+﻿// =======================================================================================
+// DatabaseLayerMySQL
+// by Weaver (Fhiz)
+// MIT licensed
+// =======================================================================================
+
+using wovencode;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace wovencode
+{
+	
+	// ===================================================================================
+	// DatabaseLayerMySQL
+	// ===================================================================================
+	[System.Serializable]
+	public partial class DatabaseLayerMySQL : DatabaseAbstractionLayer
+	{
+		
+		// -------------------------------------------------------------------------------
+
+	}
+
+}
+
+// =======================================================================================
---
+++ Scripts/Layers/DatabaseLayerSQLite.cs
@@ -0,0 +1,45 @@
+﻿// =======================================================================================
+// DatabaseLayerSQLite
+// by Weaver (Fhiz)
+// MIT licensed
+// =======================================================================================
+
+using wovencode;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using SQLite;
+
+namespace wovencode
+{
+	
+	// ===================================================================================
+	// DatabaseLayerSQLite
+	// ===================================================================================
+	[System.Serializable]
+	public partial class DatabaseLayerSQLite : DatabaseAbstractionLayer
+	{
+		
+		[Header("Options")]
+		public string databaseName 	= "Database.sqlite";
+		
+		
+		[Tooltip("Launch automatically when the game is started (recommended for Single-Player games).")]
+		public bool initOnAwake;
+		[Tooltip("Compares the hash of the database with player prefs to prevent cheating.")]
+		public bool checkIntegrity;
+		
+		
+		protected 			SQLiteConnection 	connection;
+		protected static 	string 				_dbPath = "";
+		
+		
+		
+		// -------------------------------------------------------------------------------
+
+	}
+
+}
+
+// =======================================================================================
